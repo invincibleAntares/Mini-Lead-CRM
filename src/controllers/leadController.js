0b55edd1ec455f1d63948c62260c0d85a9702034
invincibleAntares <kumarabhishek2928@gmail.com>
2026-04-28 23:26:18 +0530

    Add GET endpoints for all leads and single lead by ID

--- src/controllers/leadController.js
+++ src/controllers/leadController.js
@@ -20,6 +20,36 @@ const createLead = async (req, res, next) => {
   }
 };
 
+// Get all leads with optional status filter
+const getAllLeads = async (req, res, next) => {
+  try {
+    const { status } = req.query;
+    const filter = status ? { status } : {};
+    
+    const leads = await Lead.find(filter);
+    res.json(leads);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get single lead by ID
+const getLeadById = async (req, res, next) => {
+  try {
+    const lead = await Lead.findById(req.params.id);
+    
+    if (!lead) {
+      return res.status(404).json({ error: 'Lead not found' });
+    }
+    
+    res.json(lead);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
-  createLead
+  createLead,
+  getAllLeads,
+  getLeadById
 };
--- src/routes/leadRoutes.js
+++ src/routes/leadRoutes.js
@@ -3,7 +3,9 @@ const leadController = require('../controllers/leadController');
 
 const router = express.Router();
 
-//  CREATE endpoint
+// CREATE and READ endpoints
 router.post('/', leadController.createLead);
+router.get('/', leadController.getAllLeads);
+router.get('/:id', leadController.getLeadById);
 
 module.exports = router;
